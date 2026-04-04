51b42e8a62a801a3dd81031dd22283764eefaa9e
TimeDevFelipe <TimeDevFelipe@users.noreply.github.com>
2026-04-03 21:05:04 -0300

    adicionando autenticacao com limite de 3 tentativas e bloqueio de acesso

--- FiapBankAtm.java
+++ FiapBankAtm.java
@@ -37,6 +37,36 @@ public class FiapBankAtm {
             }
         }
 
+        // autenticacao - o usuario tem no maximo 3 tentativas
+        System.out.println("\nagora vamos fazer o login");
+        int tentativas = 0;
+        boolean autenticado = false;
+
+        while (tentativas < 3) {
+            System.out.print("digite sua senha: ");
+            String senhaDigitada = scanner.nextLine();
+
+            // usando .equals() pra comparar strings, nunca ==
+            if (senhaDigitada.equals(senhaForte)) {
+                autenticado = true;
+                System.out.println("acesso liberado! bem-vindo, " + primeiroNome);
+                break;
+            } else {
+                tentativas++;
+                int restantes = 3 - tentativas;
+                if (restantes > 0) {
+                    System.out.println("senha incorreta! voce tem " + restantes + " tentativa(s) restante(s)");
+                }
+            }
+        }
+
+        // se errou as 3 vezes, bloqueia o acesso
+        if (!autenticado) {
+            System.out.println("ACESSO BLOQUEADO");
+            scanner.close();
+            return;
+        }
+
         scanner.close();
     }
 }
